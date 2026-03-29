8f345cba3cbe1592ea4d80c4f051006460ce9913
Vikash.R <vpatanvadiya2022@gmail.com>
2026-03-30 00:56:39 +0530

    feat: replace YouTube demo with Vimeo embed

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -307,11 +307,13 @@ export default function LandingPage() {
             </div>
 
             {/* Video */}
-            <div className="aspect-video bg-gray-900">
+            <div className="relative" style={{ paddingTop: "56.25%" }}>
               <iframe
-                src="https://www.youtube.com/embed/i5V6dnPXCqI"
-                className="w-full h-full"
-                allowFullScreen
+                src="https://player.vimeo.com/video/1178239755?badge=0&autopause=0&player_id=0&app_id=58479"
+                frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
                 title="MediaLayer Demo"
               />
             </div>
